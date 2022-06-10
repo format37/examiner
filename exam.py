a7afc98d50ca0ac98b9046b82342c9ee7c02ad0c
Alexey Yurasov <13106906+format37@users.noreply.github.com>
2022-06-10 17:06:45 +0300

    Add files via upload

--- exam.py
+++ exam.py
@@ -13,12 +13,12 @@ import websockets
 import wave
 
 
-def record_audio(file_name):
+def record_audio(file_name, current_question):
     """
     Record audio file from PC microphone
     Until Space key not pressed
     :param file_name: name of audio file
-    :return: None
+    :return: Escape key pressed
     """
     pygame.init()
     pygame.display.set_mode((100, 100))
@@ -32,6 +32,8 @@ def record_audio(file_name):
     p = pyaudio.PyAudio()
 
     print('Recording')
+
+    escape = False
 
     stream = p.open(format=sample_format,
                     channels=channels,
@@ -48,7 +50,7 @@ def record_audio(file_name):
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_ESCAPE:
                     print('Finished recording')
 
                     stream.stop_stream()
@@ -62,6 +64,10 @@ def record_audio(file_name):
                     wf.writeframes(b''.join(frames))
                     wf.close()
                     recording = False
+
+                    if event.key == pygame.K_ESCAPE:
+                        escape = True
+
                     break
 
     print('Finished recording')
@@ -76,6 +82,8 @@ def record_audio(file_name):
     wf.setframerate(fs)
     wf.writeframes(b''.join(frames))
     wf.close()
+
+    return escape
 
 
 def tts(engine, tts_text):
@@ -174,16 +182,20 @@ def main():
     with open('config.json', 'r') as f:
         config = json.load(f)
 
-    # paraphrase init
-    paraphrase_server = config['paraphrase_server']
-    wait_for_server_be_ready(paraphrase_server)
-
-    # textqa init
-    textqa_server = config['textqa_server']
-    wait_for_server_be_ready(textqa_server)
-
     # stt server init
     stt_server = config['stt_server']
+
+    # evaluation init
+    do_evaluation = config['evaluation']
+
+    if do_evaluation:
+        # paraphrase init
+        paraphrase_server = config['paraphrase_server']
+        wait_for_server_be_ready(paraphrase_server)
+
+        # textqa init
+        textqa_server = config['textqa_server']
+        wait_for_server_be_ready(textqa_server)
 
     # Text to speech init
     engine = pyttsx3.init()
@@ -206,55 +218,66 @@ def main():
         questions.append(examiner_text)
         tts(engine, examiner_text)
         # record audio
-        record_audio('user.wav')
+        escape = record_audio('user.wav', current_question)
         # transcribe and receive response
         user_text = asyncio.run(stt(stt_server, 'user.wav'))
         print('user:', user_text)
         answers.append(user_text)
         prompt += '\nCandidate: ' + user_text + '\nHr: '
         current_question -= 1
+        if escape:
+            print('Escape pressed')
+            break
 
     # === Evaluate
+    if do_evaluation:
+        # textqa
+        text = ' '.join(answers)
+        texts = []
+        for _ in range(len(questions)):
+            texts.append(text)
 
-    # textqa
-    text = ' '.join(answers)
-    texts = []
-    for _ in range(len(questions)):
-        texts.append(text)
+        request = {'texts': texts, 'questions': questions}
+        request_str = json.dumps(request)
+        response = requests.post(textqa_server+'/inference', json=request_str)
+        response = json.loads(response.text)
 
-    request = {'texts': texts, 'questions': questions}
-    request_str = json.dumps(request)
-    response = requests.post(textqa_server+'/inference', json=request_str)
-    response = json.loads(response.text)
+        for i in range(len(response[0])):
+            print(questions[i], '->', response[0][i], '\n')
 
-    for i in range(len(response[0])):
-        print(questions[i], '->', response[0][i], '\n')
+        # Paraphrases
+        text_a = []
+        text_b = []
+        for i in range(len(response[0])):
+            text_a.append(answers[i])
+            text_b.append(response[0][i])
 
-    # Paraphrases
-    text_a = []
-    text_b = []
-    for i in range(len(response[0])):
-        text_a.append(answers[i])
-        text_b.append(response[0][i])
+        data = {'text_a': text_a, 'text_b': text_b}
+        request_str = json.dumps(data)
+        response = requests.post(
+            paraphrase_server+'/inference',
+            json=request_str
+            )
+        paraphrase_eval = string_to_array(response.text)
 
-    data = {'text_a': text_a, 'text_b': text_b}
-    request_str = json.dumps(data)
-    response = requests.post(paraphrase_server+'/inference', json=request_str)
-    paraphrase_eval = string_to_array(response.text)
+        for i in range(len(paraphrase_eval)):
+            print('+' if paraphrase_eval[i] else '-', questions[i], 'answer:')
+            print(answers[i], '\n')
 
-    for i in range(len(paraphrase_eval)):
-        print('+' if paraphrase_eval[i] else '-', questions[i], 'answer:')
-        print(answers[i], '\n')
+        print('Answers length: ', len(text))
+        print('Words count: ', len(text.split(' ')))
+        print('Unique words count: ', len(set(text.split(' '))))
 
-    print('Answers length: ', len(text))
-    print('Words count: ', len(text.split(' ')))
-    print('Unique words count: ', len(set(text.split(' '))))
-
-    correct = int(sum(paraphrase_eval)*100/len(paraphrase_eval))
-    examiner_text = 'You have '+str(correct)+' percent of correct answers. '
-    print(examiner_text)
-    examiner_text += 'Good job!' if correct >= 70 else 'You need to improve!'
-    tts(engine, examiner_text)
+        correct = int(sum(paraphrase_eval)*100/len(paraphrase_eval))
+        examiner_text = 'You have '
+        examiner_text += str(correct)
+        examiner_text += ' percent of correct answers. '
+        print(examiner_text)
+        if correct >= 70:
+            examiner_text += 'Good job!'
+        else:
+            examiner_text += 'You need to improve!'
+        tts(engine, examiner_text)
 
 if __name__ == '__main__':
     main()
